c49b148554ebe317a47d62176fb7f7893ebdc7ec
Kapil Dagar <kapildagar600@gmail.com>
2025-01-30 22:17:45 +0530

    added the header component and updated the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className="bg-zinc-700 text-red-500">
-      <h1>Hello</h1>
-    </div>
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/sign-in" element={<SignIn/>} />
+    <Route path="/sign-up" element={<SignUp/>} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/profile" element={<Profile/>} />
+  </Routes>
+  </BrowserRouter>
   )
 }
 
